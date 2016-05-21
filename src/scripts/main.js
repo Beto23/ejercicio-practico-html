4b2d213e1d66cf3f77586a953dbe2b49d9a1d1c7
Beto23 <josee-45@hotmail.com>
2016-05-21 14:38:35 -0500

    Agregar boton menu(nav) y scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,3 +5,34 @@ var hwindow = $(window).height();
 $('.full-windows').css({
   height: hwindow
 });
+
+if (window.scrollY == 0) {
+    $('#nav-opciones').addClass('.show-items');
+}
+var opcionesNav = $('body').find('#nav-opciones');
+var nav = $('nav');
+var menu_icon = $('#menu-icon');
+
+$(window).scroll(function(){
+  var scrolliny = $(window).scrollTop();
+  if (scrolliny > 0){
+    opcionesNav.addClass('show-items');
+    nav.addClass('padding-min');
+    menu_icon.removeClass('show-items');
+    var tamañoHeader = $('header').height();
+    $('.historia').css({
+      height: (hwindow - tamañoHeader)
+    });
+  }else {
+    opcionesNav.removeClass('show-items');
+    nav.removeClass('padding-min');
+    menu_icon.addClass('show-items');
+  }
+});
+
+$('nav').find('.icon-menu').click(function(){
+  opcionesNav.removeClass('show-items');
+  menu_icon.addClass('show-items');
+  nav.removeClass('padding-min');
+
+});
