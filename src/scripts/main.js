364356bce94efb0c36bfd145e6b734d6000cbb46
Beto23 <josee-45@hotmail.com>
2016-05-22 19:18:43 -0500

    Agregar media query header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,27 +10,43 @@ $('.full-windows').css({
 var opcionesNav = $('body').find('#nav-opciones');
 var nav = $('nav');
 var menu_icon = $('#menu-icon');
+var tamañoHeader = $('header').height();
+$('.seccion').css({
+  height: (hwindow)
+});
+var desktop = $(window).width();
 
-$(window).scroll(function(){
-  var scrolliny = $(window).scrollTop();
-  if (scrolliny > 0){
-    opcionesNav.addClass('show-items');
-    nav.addClass('padding-min');
-    menu_icon.removeClass('show-items');
-    var tamañoHeader = $('header').height();
-    $('.seccion').css({
-      height: (hwindow)
-    });
-  }else {
+$(window).width(function(){
+var desktop = $(window).width();
+});
+
+if (desktop > 847) {
+  $(window).scroll(function(){
+    var scrolliny = $(window).scrollTop();
+    if (scrolliny > 0){
+      opcionesNav.addClass('show-items');
+      nav.addClass('padding-min');
+      menu_icon.removeClass('show-items');
+
+    }else {
+      opcionesNav.removeClass('show-items');
+      nav.removeClass('padding-min');
+      menu_icon.addClass('show-items');
+    }
+  });
+  $('nav').find('.icon-menu').click(function(){
     opcionesNav.removeClass('show-items');
-    nav.removeClass('padding-min');
     menu_icon.addClass('show-items');
-  }
-});
+    nav.removeClass('padding-min');
 
-$('nav').find('.icon-menu').click(function(){
-  opcionesNav.removeClass('show-items');
-  menu_icon.addClass('show-items');
-  nav.removeClass('padding-min');
+  });
+}else {
+  nav.addClass('padding-min');
+  menu_icon.removeClass('show-items');
 
-});
+  $('nav').find('.icon-menu').click(function(){
+    opcionesNav.toggleClass('on-menu');
+    opcionesNav.find('a').toggleClass('col-xs-6');
+    $('#menu-icon').toggleClass('menu-barraOn');
+  });
+}
